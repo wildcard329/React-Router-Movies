803cba8be07845e0346e967db09e8f0fe5abb945
wildcard329 <enkiduwildman@gmail.com>
2020-02-25 12:57:43 -0800

    added dependencies, wrapped appilcation in router, mounted components in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/' component={MovieList} />
+      <Route path='/movies/:id' component={Movie} />
     </div>
   );
 };
