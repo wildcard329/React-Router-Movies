6737e2239f1758fa168f78675778666dd01880ac
wildcard329 <enkiduwildman@gmail.com>
2020-02-25 18:54:14 -0800

    added history to the hone button via the useHistory hook

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-// const history = useHistory();
-// const goHome = () => {
-//   history.push('/home')
-// }
 
-const SavedList = props => (
-  <div className="saved-list">
+const SavedList = props => {
+  let history = useHistory();
+  const goHome = () => {
+    history.push('/')
+  }
+  return (
+    <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div onClick={goHome} className="home-button">Home</div>
   </div>
-);
+  )
+};
 
 export default SavedList;
